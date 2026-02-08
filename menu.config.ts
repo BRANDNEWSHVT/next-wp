edeac6655a4d05fa4a9c42cf2b2f3ace036a098f
Adriana Eka Prayudha <radenadriep@gmail.com>
2026-02-09 00:32:52 +0700

    feat: first commit ⚡

---
+++ menu.config.ts
@@ -0,0 +1,12 @@
+// Define the menu items
+export const mainMenu = {
+  home: "/",
+  about: "https://github.com/brandnewshvt/next-wp",
+  blog: "/posts",
+};
+
+export const contentMenu = {
+  categories: "/posts/categories",
+  tags: "/posts/tags",
+  authors: "/posts/authors",
+};
---
+++ site.config.ts
@@ -0,0 +1,11 @@
+type SiteConfig = {
+  site_domain: string;
+  site_name: string;
+  site_description: string;
+};
+
+export const siteConfig: SiteConfig = {
+  site_name: "focus",
+  site_description: "Starter template for Next.js & WordPress",
+  site_domain: "https://focus.radenadri.xyz",
+};
